15dcb340dab76a8d85a4934dc0af66565a241329
CasseiaN <kutsalkeko2143@gmail.com>
2024-05-04 00:19:22 +0300

    Prefab Update

--- Assets/cyborgAi.cs
+++ Assets/cyborgAi.cs
@@ -5,44 +5,78 @@ using UnityEngine;
 public class cyborgAi : MonoBehaviour
 {
     public GameObject Player;
+    public GameObject hurtBox;
     public Transform target;
     public Animator anim;
+
+    public float attackDelay;
+    public float attackDelay2;
     public bool isAwake = false;
-    public float speed = 5;
+    public bool canMove = true;
+    public bool attackOnCooldown = false;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float step = speed * Time.deltaTime;
-        if(isAwake == true)
+        if (isAwake)
         {
-
-                        // Hedefin pozisyonunu al
-            Vector3 targetPosition = target.position;
-
-            // Yalnızca x ekseni boyunca takip etmek için y ve z pozisyonlarını sabit tut
-            targetPosition.y = transform.position.y;
-            targetPosition.z = transform.position.z;
-
-            if (target.position.x < transform.position.x)
+            if (canMove)
             {
-                // Hedef soldaysa sola dön
-                transform.rotation = Quaternion.Euler(0, 180, 0); // Karakteri 180 derece döndürür (sola)
-            }
-            else
-            {
-                // Hedef sağdaysa sağa dön
-                transform.rotation = Quaternion.Euler(0, 0, 0); // Karakteri orijinal dönüşüne geri getirir (sağa)
-            }
+                // Hedefin pozisyonunu al
+                Vector3 targetPosition = target.position;
 
-            // Takip eden GameObject'in pozisyonunu yavaşça hedefin pozisyonuna doğru güncelle
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+                // Yalnızca x ekseni boyunca takip etmek için y ve z pozisyonlarını sabit tut
+                targetPosition.y = transform.position.y;
+                targetPosition.z = transform.position.z;
+
+                if (target.position.x < transform.position.x)
+                {
+                    // Hedef soldaysa sola dön
+                    transform.rotation = Quaternion.Euler(0, 180, 0); // Karakteri 180 derece döndürür (sola)
+                }
+                else
+                {
+                    // Hedef sağdaysa sağa dön
+                    transform.rotation = Quaternion.Euler(0, 0, 0); // Karakteri orijinal dönüşüne geri getirir (sağa)
+                }
+
+                // Takip eden GameObject'in pozisyonunu yavaşça hedefin pozisyonuna doğru güncelle
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+            }
 
         }
+    }
+
+    public void Attack()
+    {
+        if (attackOnCooldown == false)
+        {
+            attackOnCooldown = true;
+            StartCoroutine(IEnuAttack());
+        }
+
+
+    }
+
+    IEnumerator IEnuAttack()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(attackDelay);
+        hurtBox.SetActive(true);
+        Debug.Log("hit");
+        yield return new WaitForSeconds(0.1f);
+        hurtBox.SetActive(false);
+        yield return new WaitForSeconds(attackDelay2);
+        attackOnCooldown = false;
+        canMove = true;
+
     }
 }
