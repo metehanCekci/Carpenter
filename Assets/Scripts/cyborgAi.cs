95fdaf1eeeed1ec1224a77fa245e04522c05ca8b
integerfan66 <kaan27043@gmail.com>
2024-05-04 12:44:08 +0300

    yer çekimi değiştirme script'i yapıldı (sadece oyuncu için çalışıyor neden bilmiyorum)
    İmdat.

--- Assets/Scripts/cyborgAi.cs
+++ Assets/Scripts/cyborgAi.cs
@@ -38,8 +38,10 @@ public class cyborgAi : MonoBehaviour
                 Vector3 targetPosition = target.position;
 
                 // Yalnızca x ekseni boyunca takip etmek için y ve z pozisyonlarını sabit tut
-                targetPosition.y = transform.position.y;
-                targetPosition.z = transform.position.z;
+               
+                 targetPosition.y = transform.position.y;
+                 targetPosition.z = transform.position.z;
+               
 
                 if (target.position.x < transform.position.x)
                 {
