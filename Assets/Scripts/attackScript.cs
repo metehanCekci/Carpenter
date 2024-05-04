2b26fed9efd7e3506965804a8222654f9b048cc0
Metehan <metehancekci9@gmail.com>
2024-05-04 06:42:55 +0300

    sa

--- Assets/Scripts/attackScript.cs
+++ Assets/Scripts/attackScript.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class attackScript : MonoBehaviour
 {
-    private void Start()
+    Animator anim;
+    public GameObject attackPoint;
+    public float radius;
+    public LayerMask enemies;
+    void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
     void Update()
     {
-
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isAttacking", true);
+            Debug.Log("Saldýrdý!");
+        }
+    }
+    public void endAttack()
+    {
+        anim.SetBool("isAttacking", false);
+    }
+    public void attack()
+    {
+        Collider2D[] enemy = Physics2D.OverlapCircleAll(attackPoint.transform.position, radius, enemies);
     }
 }
