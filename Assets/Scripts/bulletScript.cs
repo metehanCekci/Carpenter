c976426f769d6b79a69a26614674e8d38da6baf9
CasseiaN <kutsalkeko2143@gmail.com>
2024-05-04 08:57:49 +0300

    sss

--- Assets/Scripts/bulletScript.cs
+++ Assets/Scripts/bulletScript.cs
@@ -6,6 +6,7 @@ public class bulletScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 5;
+    public bool goingLeft = false;
     void Start()
     {
         
@@ -14,7 +15,10 @@ public class bulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         this.transform.position += transform.right * speed * Time.deltaTime;
+        
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
