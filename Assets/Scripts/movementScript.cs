0a4ad8523ac06f265c55db449305999f9abd0650
Metehan <metehancekci9@gmail.com>
2024-05-04 03:21:55 +0300

    sa

--- Assets/Scripts/movementScript.cs
+++ Assets/Scripts/movementScript.cs
@@ -6,19 +6,26 @@ using UnityEngine.InputSystem;
 public class movementScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    public Animator animator;
+    private Animator animator;
 
     private float horizontal;
     public float speed=3.0f;
     public float jumpingPower = 5f;
     public bool isFacingRight = true;
 
+    public float shortJumpForce = 10f; // Kýsa zýplama kuvveti
+    public float longJumpForce = 20f; // Uzun zýplama kuvveti
+    public float maxJumpTime = 0.5f; // Maksimum zýplama süresi
+    public float jumpTime = 0f; // Geçerli zýplama süresi
+    private bool isJumping = false; // Zýplama durumu
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -37,8 +44,37 @@ public class movementScript : MonoBehaviour
         }
         animator.SetBool(name: "onAir", value: !isGrounded());
         animator.SetBool(name: "isWalking", value: Mathf.Abs(horizontal) > 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            jumpTime = 0f;
+            Jump(shortJumpForce);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTime < maxJumpTime)
+            {
+                jumpTime += Time.deltaTime;
+                // Zýplama süresine göre zýplama mesafesini artýr
+                float jumpForce = Mathf.Lerp(shortJumpForce, longJumpForce, jumpTime / maxJumpTime);
+                rb.velocity = new Vector2(rb.velocity.x, 0f);
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && isJumping)
+        {
+            isJumping = false;
+        }
     }
- 
+    void Jump(float force)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0f);
+        rb.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
+
     public void Jump(InputAction.CallbackContext context)
     {
         if (context.performed && isGrounded()) 
