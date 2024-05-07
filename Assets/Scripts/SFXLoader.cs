c772be97a67b27a9a649c7d4a7df471938c0689f
CasseiaN <kutsalkeko2143@gmail.com>
2024-05-07 19:13:37 +0300

    asd

--- Assets/Scripts/SFXLoader.cs
+++ Assets/Scripts/SFXLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class SFXLoader : MonoBehaviour
@@ -8,6 +9,8 @@ public class SFXLoader : MonoBehaviour
     public AudioClip hit;
     public AudioClip hurt;
     public AudioClip cyborgShoot;
+
+    public AudioClip dash;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +36,11 @@ public class SFXLoader : MonoBehaviour
     public void playCyborgShoot()
     {
         aS.PlayOneShot(cyborgShoot);
+    }
+
+    public void playDash()
+    {
+        aS.PlayOneShot(dash);
     }
 
 
