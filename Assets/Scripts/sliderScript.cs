d0fa7ba118647599abf23cbfafcac6b0faa4b3b8
integerfan66 <kaan27043@gmail.com>
2024-05-14 22:14:52 +0300

    imdat

--- Assets/Scripts/sliderScript.cs
+++ Assets/Scripts/sliderScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using Unity.VisualScripting;
 
 [RequireComponent(typeof(Slider))]
 
@@ -18,22 +19,32 @@ public class sliderScript : MonoBehaviour
     public Text volumeLabel;
     [SerializeField]
     public string volumeName;
-
+    public float defaultValue = 100f;
     private void Start()
     {
-        UpdateValueOnChange(slider.value);
+        // Load the saved slider value from PlayerPrefs
+        float savedValue = PlayerPrefs.GetFloat(volumeName, defaultValue);
+        volumeLabel.text = Mathf.Round(savedValue * 100.0f).ToString() + "%";
+        slider.value = savedValue;
+        
 
+        // Add listener to the slider value change event
         slider.onValueChanged.AddListener(delegate
         {
             UpdateValueOnChange(slider.value);
         });
     }
+
     public void UpdateValueOnChange(float value)
     {
-        if (mixer != null) 
-            mixer.SetFloat(volumeName, Mathf.Log(value)* 20f);
+        if (mixer != null)
+            mixer.SetFloat(volumeName, Mathf.Log(value) * 20f);
 
         if (volumeLabel != null)
             volumeLabel.text = Mathf.Round(value * 100.0f).ToString() + "%";
+
+        // Save the changed value to PlayerPrefs
+        PlayerPrefs.SetFloat(volumeName, value);
+        PlayerPrefs.Save(); // Save immediately to ensure the value is stored
     }
 }
